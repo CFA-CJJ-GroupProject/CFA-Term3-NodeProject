f46c812adc6994135194f29d0a8fffcc9897a2a5
Samseppiol <j_hage@live.com>
2017-07-05 15:02:22 +1000

    Created jobs schema, validated return of JSON data

--- server.js
+++ server.js
@@ -16,7 +16,38 @@ server.get('/jobs', (req, res) => {
 
 
 server.get('/setup', (req, res) => {
-    const setupData = new Job({ title: 'test for boiler plate setup' })
+    const setupData = new Job({ 
+      jobNumber: "ASP3734ytiuwrgiu",
+
+      pickupAddress: "123 fake street",
+      pickupDate: 12/02/2016,
+      pickupTime: 1245,
+      pickupInstructions: "Avoid Jay in the morning before he has had a cigarette",
+
+      deliveryAddress: "TEST DUMMY DATA",
+      deliveryDate: 13/02/2016,
+      deliveryTime: 1200,
+      deliveryInstructions: "Avoid Jack before he has had a coffee and possibly a cigarette",
+
+      descriptionOfGoods: "TEST DUMMY DATA",
+      deliveryType: "TEST DUMMY DATA",
+
+      driverId: 5, // must not be empty
+      businessId: 5, // must not be empty
+
+      status: "TEST DUMMY DATA",
+
+      dimensions: {
+        height: 5,
+        length: 5,
+        width: 5
+      }, 
+
+      weight: 5
+
+    })
+
+
     console.log('Save')
     setupData.save()
         .then(doc => {
