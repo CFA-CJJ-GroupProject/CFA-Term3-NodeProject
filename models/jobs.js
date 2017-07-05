4f7d2d7b4c3288fdb2968508973d9cc1df9092f2
Samseppiol <j_hage@live.com>
2017-07-05 15:02:22 +1000

    Created Users and customer schema, validated return of JSON data

--- models/jobs.js
+++ models/jobs.js
@@ -1,7 +1,35 @@
 const mongoose = require('./init')
 
 const jobSchema = mongoose.Schema({
-    title: String
+
+  jobNumber: String, //Must be unique
+
+  pickupAddress: String,
+  pickupDate: Date,
+  pickupTime: Date,
+  pickupInstructions: String,
+
+  deliveryAddress: String,
+  deliveryDate: Date,
+  deliveryTime: Date,
+  deliveryInstructions: String,
+
+  descriptionOfGoods: String,
+  deliveryType: String,
+
+  driverId: Number, // must not be empty
+  businessId: Number, // must not be empty
+
+  status: String,
+
+  dimensions: {
+    height: Number,
+    length: Number,
+    width: Number
+  }, 
+
+  weight: Number
+
 })
 
 const Job = mongoose.model('Job', jobSchema)
