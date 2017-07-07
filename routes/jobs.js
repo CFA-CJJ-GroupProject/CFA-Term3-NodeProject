863a6806a8f3b36a867844fae0cc82e7e2bd1087
Samseppiol <j_hage@live.com>
2017-07-07 12:13:38 +1000

    Removing commented out code

--- routes/jobs.js
+++ routes/jobs.js
@@ -2,50 +2,6 @@ const express = require('express')
 const Job = require('../models/job')
 const router = express.Router()
 
-
-
-// // READ OF CRUD
-// router.get('/jobs', (req, res) => {
-//     Job.find()
-//   // What we are wanting to be called
-//     .then(jobs => {
-//       res.json(jobs)
-//     })
-//     // if something goes wrong
-//     .catch(error => {
-//       res.status(500).json({ error: error })
-//     })
-// })
-
-
-
-// // Creating New Job
-// router.post('/jobs', (req, res) => {
-//   const newJob = req.body
-//   Job.create(newJob)
-//     .then (job => {
-//       res.json(job)
-//     })
-//     .catch(error => {
-//       res.status(500).json({ error: error})
-//     })
-// })
-
-
-
-// module.exports = router
-
-
-//   Job.find().then(jobs => {
-//     res.json(jobs)
-//   }).then(error => {
-//     res.json({error})
-//   })
-// })
-
-
-
-
 // READ OF CRUD
 router.get('/jobs', (req, res) => {
     Job.find()
