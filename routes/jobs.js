dd6c470ebd45324d7d0c3d11d61585ef511c102a
Samseppiol <j_hage@live.com>
2017-07-07 12:12:13 +1000

    crud implemented for jobs

--- routes/jobs.js
+++ routes/jobs.js
@@ -4,18 +4,61 @@ const router = express.Router()
 
 
 
+// // READ OF CRUD
+// router.get('/jobs', (req, res) => {
+//     Job.find()
+//   // What we are wanting to be called
+//     .then(jobs => {
+//       res.json(jobs)
+//     })
+//     // if something goes wrong
+//     .catch(error => {
+//       res.status(500).json({ error: error })
+//     })
+// })
+
+
+
+// // Creating New Job
+// router.post('/jobs', (req, res) => {
+//   const newJob = req.body
+//   Job.create(newJob)
+//     .then (job => {
+//       res.json(job)
+//     })
+//     .catch(error => {
+//       res.status(500).json({ error: error})
+//     })
+// })
+
+
+
+// module.exports = router
+
+
+//   Job.find().then(jobs => {
+//     res.json(jobs)
+//   }).then(error => {
+//     res.json({error})
+//   })
+// })
+
+
+
+
 // READ OF CRUD
 router.get('/jobs', (req, res) => {
     Job.find()
   // What we are wanting to be called
-    .then(jobs => {
-      res.json(jobs)
+    .then(job => {
+      res.json(job)
     })
     // if something goes wrong
     .catch(error => {
       res.status(500).json({ error: error })
     })
 })
+
 
 
 // Pulling out via given ID, 
@@ -31,6 +74,7 @@ router.get('/jobs/:id', (req, res) => {
 })
 
 
+
 // Creating New Job
 router.post('/jobs', (req, res) => {
   const newJob = req.body
@@ -43,15 +87,43 @@ router.post('/jobs', (req, res) => {
     })
 })
 
+router
+.param('id', (req, res, next, id) => {
+    req.itemQuery = Job.findById(id)
+    next()
+})
+
+router.route('/jobs/:id')
+.get((req, res) => {
+    req.itemQuery
+        .then(job => {
+            res.json(job)
+        })
+        .catch(error => {
+            res.status(404).json({ error })
+        })
+})
+
+.put((req, res) => {
+    const newJob = req.body
+    req.itemQuery.update(newJob)
+        .then(() => {
+            res.json(newJob)
+        })
+        .catch(error => {
+            res.status(404).json({ error })
+        })
+})
+.delete((req, res) => {
+    req.itemQuery.remove()
+        .then(() => {
+            res.status(204).json({})
+        })
+        .catch(error => {
+            res.status(404).json({ error })
+        })
+})
+
 
 
 module.exports = router
-
-
-//   Job.find().then(jobs => {
-//     res.json(jobs)
-//   }).then(error => {
-//     res.json({error})
-//   })
-// })
-
