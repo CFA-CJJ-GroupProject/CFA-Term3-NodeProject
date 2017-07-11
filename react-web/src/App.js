04a2d7dfdff6e3958509860cd22391220ebb25de
Jay <jayalexander@outlook.com.au>
2017-07-11 20:36:16 +1000

    Added Routes and pages for Driver and Client

--- react-web/src/App.js
+++ react-web/src/App.js
@@ -7,6 +7,12 @@ import {
 import logo from './logo.svg';
 import './App.css';
 import CreateJobPage from './pages/CreateJobPage'
+import JobsPage from './pages/JobsPage'
+import JobConfirmationPage from './pages/JobConfirmationPage'
+import JobCard from './pages/JobCard'
+import CreateUserPage from './pages/CreateUserPage'
+import UsersPage from './pages/UsersPage'
+
 
 class App extends Component {
 
@@ -24,15 +30,42 @@ class App extends Component {
     return (
       <Router>
         <main>
+          <Route path='/createjob' render={
+            () => (
+              <CreateJobPage/>
+            )
+          } />
 
+          <Route path='/jobs' render={
+            () => (
+              <JobsPage/>
+            )
+          } />
 
+          <Route path='/jobconfirmation' render={
+            () => (
+              <JobConfirmationPage/>
+            )
+          } />
 
-            <Route path='/createjob' render={
-              () => (
-                <CreateJobPage/>
-              )
-            } />
+          <Route path='/jobcard/:id' render={
+            () => (
+              <JobCard/>
+            )
+          } />
 
+          <Route path='/creatuser' render={
+            () => (
+              <CreateUserPage/>
+            )
+          } />
+
+          <Route path='/userspage' render={
+            () => (
+              <UsersPage/>
+            )
+          } />
+          
         </main>
       </Router>
     )
--- react-web/src/components/readAndClearForm.js
+++ react-web/src/components/readAndClearForm.js
@@ -2,6 +2,7 @@ function readAndClearForm(form) {
     const elements = form
     let values = {}
     // Loop through form elements https://stackoverflow.com/a/19978872
+    // eslint-disable-next-line
     for (let i = 0, element; element = elements[i++];) {
         // Read value from <input>
         values[element.name] = element.value
