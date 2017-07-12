f181a77dc953227721257c8ab54f6c3261514449
paigerichards <paige.richards14@hotmail.co.uk>
2017-07-12 14:31:23 +1000

    Requiring form feilds for added validation

--- react-web/src/components/CreateJobForm.js
+++ react-web/src/components/CreateJobForm.js
@@ -49,46 +49,46 @@ export default function CreateJobForm({
       <div className='cj-left'>
         <div className='pickup'>
           <label>Pickup Infomation:</label>
-          <Field type='text' placeholder='Street...' name='pickupStreet' />
-          <Field type='text' placeholder='Suburb...' name='pickupSuburb' />
+          <Field required='true' type='text' placeholder='Street...' name='pickupStreet' />
+          <Field required='true' type='text' placeholder='Suburb...' name='pickupSuburb' />
           <div className='postcode-state'>
           <div className='postcode'>
-          <Field type='text' placeholder='Postcode...' name='pickupPostcode' />
+          <Field required='true' type='text' placeholder='Postcode...' name='pickupPostcode' />
           </div>
           <div className='state'>
-          <SelectStateField name="pickupState"/>
+          <SelectStateField required='true' name="pickupState"/>
           </div>
         </div>
 
-          <Field type='date' name='pickupDate' />
-          <Field type='time' name='pickupTime' />
-          <TextField placeholder='Instructions..' name='pickupInstructions' />
+          <Field required='true' type='date' name='pickupDate' />
+          <Field required='true' type='time' name='pickupTime' />
+          <TextField required='true' placeholder='Instructions..' name='pickupInstructions' />
         </div>
         <div className='description'>
           <label>Description of Item(s):</label>
-          <TextField placeholder='Description of goods...' name='descriptionOfGoods' />
+          <TextField required='true' placeholder='Description of goods...' name='descriptionOfGoods' />
         </div>
       </div>
 
       <div className="cj-right">
         <div className='delivery'>
           <label>Delivery Infomation:</label>
-          <Field type='text' placeholder='Street...' name='deliveryStreet' />
-          <Field type='text' placeholder='Suburb...' name='deliverySuburb' />
-          <Field type='text' placeholder='Postcode...' name='deliveryPostcode' />
-          <SelectStateField name="deliveryState"/>
-          <Field type='date' name='deliveryDate' />
-          <Field type='time' name='deliveryTime' />
-          <TextField placeholder='Instructions..' name='deliveryInstructions' type=''/>
+          <Field required='true' type='text' placeholder='Street...' name='deliveryStreet' />
+          <Field required='true' type='text' placeholder='Suburb...' name='deliverySuburb' />
+          <Field required='true' type='text' placeholder='Postcode...' name='deliveryPostcode' />
+          <SelectStateField required='true' name="deliveryState"/>
+          <Field required='true' type='date' name='deliveryDate' />
+          <Field required='true' type='time' name='deliveryTime' />
+          <TextField required='true' placeholder='Instructions..' name='deliveryInstructions' type=''/>
         </div>
         <div className='product-info'>
           <label>Delivery Type and Item(s) Dimensions:</label>
-          <Field type='number' placeholder='size of mm' name='dimensions{length}' />
-          <Field type='number' placeholder='size of mm' name='dimensions{width}' />
-          <Field type='number' placeholder='size of mm' name='dimensions{height}' />
+          <Field required='true' type='number' placeholder='size of mm' name='dimensions{length}' />
+          <Field required='true' type='number' placeholder='size of mm' name='dimensions{width}' />
+          <Field required='true' type='number' placeholder='size of mm' name='dimensions{height}' />
           {/* need to add option to add more for multiple items */}
-          <Field type='number' placeholder='kgs' name='weight' />
-          <SelectDeliveryField />
+          <Field required='true' type='number' placeholder='kgs' name='weight' />
+          <SelectDeliveryField required='true' />
         </div>
       </div>
         <button type='submit' className='cj-button'>Create Job</button>
--- react-web/src/components/TextField.js
+++ react-web/src/components/TextField.js
@@ -3,12 +3,13 @@ import React from 'react'
 export default function TextField({
   label,
   name,
-  placeholder
+  placeholder,
+  required
 }) {
   return (
     <label>
         <span>{ label } </span>
-        <textarea name={ name } placeholder={ placeholder } />
+        <textarea name={ name } placeholder={ placeholder } required={ required } />
     </label>
   )
 }
