a611989f51be84d1a5e1b3a38554fef1e58b19bf
Samseppiol <j_hage@live.com>
2017-07-13 14:57:36 +1000

    add JWT token for sign in and registering

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -1,9 +1,28 @@
 const passport = require('passport')
 const passportJWT = require('passport-jwt')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 const jwtSecret = (process.env.JWT_SECRET)
 const jwtAlgorithm = 'HS256'
+
+
+function signTokenHandler(req, res) {
+  const user = req.user
+  const token = jwt.sign(
+    {
+      username: user.username,
+      role: user.role
+    },
+    jwtSecret,
+    {
+      subject: user._id.toString(),
+      algorithm: jwtAlgorithm,
+      expiresIn: '6h'
+    }
+  )
+  res.json({ token })
+}
 
 passport.use(
   User.createStrategy()
@@ -58,4 +77,5 @@ module.exports = {
   authenticateSignIn: passport.authenticate('local', { session: false }),
   authenticateJWT: passport.authenticate('jwt', { session: false }),
   initialize: passport.initialize(),
+  signTokenHandler
 }
