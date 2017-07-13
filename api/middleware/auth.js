f956ad8294765f8458e8ba4231e20edd598ad35c
Samseppiol <j_hage@live.com>
2017-07-13 11:07:28 +1000

    added jwt requests

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -1,7 +1,34 @@
 const passport = require('passport')
+const passportJWT = require('passport-jwt')
 const User = require('../models/user')
 
-passport.use(User.createStrategy())
+const jwtSecret = (process.env.JWT_SECRET)
+const jwtAlgorithm = 'HS256'
+
+passport.use(
+  User.createStrategy()
+)
+
+passport.use(
+  new passportJWT.JwtStrategy(
+    {
+      secretOrKey: jwtSecret,
+      jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeader(),
+      algorithms: [jwtAlgorithm],
+    }, (jwtPayload, done) => {
+      const userID = jwtPayload.sub //User is the subject
+      User.findById(userID) 
+      .then(user => {
+        if (user) {
+          done(null, user)
+        }
+        else {
+          done(null, false)
+        }
+      })
+    }
+  )
+)
 
 function registerMiddleware(req, res, next) {
   const user = new User({
