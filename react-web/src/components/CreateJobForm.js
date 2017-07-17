1587cf6c3b5383fde166661468ad2c044f0546ca
Samseppiol <j_hage@live.com>
2017-07-17 16:59:22 +1000

    after a few headaches and state problems, can now create jobs on the gui and they are hitting the db WOOHOOO

--- react-web/src/components/CreateJobForm.js
+++ react-web/src/components/CreateJobForm.js
@@ -6,6 +6,78 @@ import SelectDeliveryField from './SelectDeliveryField'
 import readAndClearForm from './readAndClearForm'
 import '../style.css'
 
+function handleSubmit(event, onCreate) {
+    event.preventDefault()
+
+    const form = event.target
+    const jobNumber = form.elements['jobNumber'].value
+    const pickupStreet = form.elements['pickupStreet'].value
+    const pickupSuburb = form.elements['pickupSuburb'].value
+    const jobValues = readAndClearForm(form)
+
+    onCreate({jobNumber, pickupStreet, pickupSuburb})
+}
+
+
+
+export default function CreateJobForm({
+    onCreate
+}) {
+  return (
+    <form onSubmit={ (event) => handleSubmit(event, onCreate) }>
+      <div className='cj-left'>
+        <div className='pickup'>
+          <label>Pickup Infomation:</label>
+          <Field required='true' type='text' placeholder='Job ID' name='jobNumber' />
+          <Field required='true' type='text' placeholder='Street...' name='pickupStreet' />
+          <Field required='true' type='text' placeholder='Suburb...' name='pickupSuburb' />
+          <div className='postcode-state'>
+          <div className='postcode'>
+          <Field required='false' type='text' placeholder='Postcode...' name='pickupPostcode' />
+          </div>
+          <div className='state'>
+          <SelectStateField required='false' name="pickupState"/>
+          </div>
+        </div>
+
+          <Field required='false' type='date' name='pickupDate' />
+          <Field required='false' type='time' name='pickupTime' />
+          <TextField required='false' placeholder='Instructions..' name='pickupInstructions' />
+        </div>
+        <div className='description'>
+          <label>Description of Item(s):</label>
+          <TextField required='false' placeholder='Description of goods...' name='descriptionOfGoods' />
+        </div>
+      </div>
+
+      <div className="cj-right">
+        <div className='delivery'>
+          <label>Delivery Infomation:</label>
+          <Field required='false' type='text' placeholder='Street...' name='deliveryStreet' />
+          <Field required='false' type='text' placeholder='Suburb...' name='deliverySuburb' />
+          <Field required='false' type='text' placeholder='Postcode...' name='deliveryPostcode' />
+          <SelectStateField required='false' name="deliveryState"/>
+          <Field required='false' type='date' name='deliveryDate' />
+          <Field required='false' type='time' name='deliveryTime' />
+          <TextField required='false' placeholder='Instructions..' name='deliveryInstructions' type=''/>
+        </div>
+        <div className='product-info'>
+          <label>Delivery Type and Item(s) Dimensions:</label>
+          <Field required='false' type='number' placeholder='size of mm' name='dimensions{length}' />
+          <Field required='false' type='number' placeholder='size of mm' name='dimensions{width}' />
+          <Field required='false' type='number' placeholder='size of mm' name='dimensions{height}' />
+          {/* need to add option to add more for multiple items */}
+          <Field required='false' type='number' placeholder='kgs' name='weight' />
+          <SelectDeliveryField required='false' />
+        </div>
+      </div>
+        <button type='submit' className='cj-button'>Create Job</button>
+    </form>
+  )
+}
+
+
+
 // componentDidMount(){
 //   fetch('database goes here')
 //     .then(res => res.json())
@@ -33,66 +105,3 @@ import '../style.css'
 //       });
 //   });
 // }
-
-function submitCreate(event, onCreate) {
-    event.preventDefault()
-
-    const form = event.target
-    const jobValues = readAndClearForm(form)
-    onCreate(jobValues)
-}
-
-export default function CreateJobForm({
-    onCreate
-}) {
-  return (
-    <form onSubmit={ (event) => submitCreate(event, onCreate) }>
-      <div className='cj-left'>
-        <div className='pickup'>
-          <label>Pickup Infomation:</label>
-          <Field required='true' type='text' placeholder='Street...' name='pickupStreet' />
-          <Field required='true' type='text' placeholder='Suburb...' name='pickupSuburb' />
-          <div className='postcode-state'>
-          <div className='postcode'>
-          <Field required='true' type='text' placeholder='Postcode...' name='pickupPostcode' />
-          </div>
-          <div className='state'>
-          <SelectStateField required='true' name="pickupState"/>
-          </div>
-        </div>
-
-          <Field required='true' type='date' name='pickupDate' />
-          <Field required='true' type='time' name='pickupTime' />
-          <TextField required='true' placeholder='Instructions..' name='pickupInstructions' />
-        </div>
-        <div className='description'>
-          <label>Description of Item(s):</label>
-          <TextField required='true' placeholder='Description of goods...' name='descriptionOfGoods' />
-        </div>
-      </div>
-
-      <div className="cj-right">
-        <div className='delivery'>
-          <label>Delivery Infomation:</label>
-          <Field required='true' type='text' placeholder='Street...' name='deliveryStreet' />
-          <Field required='true' type='text' placeholder='Suburb...' name='deliverySuburb' />
-          <Field required='true' type='text' placeholder='Postcode...' name='deliveryPostcode' />
-          <SelectStateField required='true' name="deliveryState"/>
-          <Field required='true' type='date' name='deliveryDate' />
-          <Field required='true' type='time' name='deliveryTime' />
-          <TextField required='true' placeholder='Instructions..' name='deliveryInstructions' type=''/>
-        </div>
-        <div className='product-info'>
-          <label>Delivery Type and Item(s) Dimensions:</label>
-          <Field required='true' type='number' placeholder='size of mm' name='dimensions{length}' />
-          <Field required='true' type='number' placeholder='size of mm' name='dimensions{width}' />
-          <Field required='true' type='number' placeholder='size of mm' name='dimensions{height}' />
-          {/* need to add option to add more for multiple items */}
-          <Field required='true' type='number' placeholder='kgs' name='weight' />
-          <SelectDeliveryField required='true' />
-        </div>
-      </div>
-        <button type='submit' className='cj-button'>Create Job</button>
-    </form>
-  )
-}
--- react-web/src/pages/CreateJobPage.js
+++ react-web/src/pages/CreateJobPage.js
@@ -1,13 +1,46 @@
-import React from 'react'
+import React, {Component} from 'react'
 import CreateJobForm from '../components/CreateJobForm'
 
-const CreateJobPage = ({
-    onCreateJob
-}) => (
+
+
+class CreateJobPage extends Component {
+	state = {
+		jobs: null
+	}
+
+	handleCreateJob = ({ jobNumber, pickupStreet, pickupSuburb}) => {
+		console.log(jobNumber)
+		console.log(pickupStreet)
+		console.log(pickupSuburb)
+	// Post to /jobsß∂
+	fetch('/Jobs', { 
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({jobNumber, pickupStreet, pickupSuburb })
+	})
+	.then(res => res.json())
+	.then(newJob => {
+		this.setState((prevState) => {
+			// Update local state with new job
+			return {
+				newJob
+			}
+		})
+	})
+	.then(error => {
+		this.setState({ error })
+	})
+}
+  render () {
+	return (
     <div>
       <h1>Create Job</h1>
-        <CreateJobForm onCreate={ onCreateJob } />
+        <CreateJobForm onCreate={ this.handleCreateJob } />
     </div>
       )
+		}
+}
 
 export default CreateJobPage
