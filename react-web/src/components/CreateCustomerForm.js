1382f5cb19157a4b32e044f2ebbdbe934b40f379
paigerichards <paige.richards14@hotmail.co.uk>
2017-07-21 13:40:30 +1000

    Form of Create Customer, Jobs page(responsive for driver role) styled

--- react-web/src/components/CreateCustomerForm.js
+++ react-web/src/components/CreateCustomerForm.js
@@ -17,32 +17,27 @@ export default function CreateCustomerForm({
 }) {
   return (
     <form onSubmit={ (event) => submitUpdate(event, onUpdate) }>
-      <div className='cj-left'>
+      <div className='cc-form'>
+      <div className='cc-left'>
+          <label>Company information:</label>
           <Field required='true' type='username' placeholder='Username...' name='username' />
-          <Field required='true' type='text' placeholder='Contact department...' name='department' />
+          <Field required='true' type='text' placeholder='Company name' name='department' />
           <Field required='true' type='tel' placeholder='Phone Number...' name='phoneNumber' />
           <Field required='true' type='email' placeholder='Email...' name='email' />
           <Field required='false' type='tel' placeholder='Fax...' name='fax' />
-      </div>
-        <div className='cj-right'>
-        <div>
+        </div>
+
+          <div className='cc-right'>
+          <label>Transport pickups:</label>
           <Field required='true' type='text' placeholder='Street...' name='pickupStreet' />
           <Field required='true' type='text' placeholder='Suburb...' name='pickupSuburb' />
-            <ul className='measurements'>
-              <li>
                 <Field required='true' type='text' placeholder='Postcode...' name='pickupPostcode' />
-              </li>
-              <li>
                 <SelectStateField required='true' name="pickupState"/>
-              </li>
-            </ul>
-      </div>
-        </div>
+              </div>
         <div className='cc-middle'>
           <TextField required='false' placeholder='Notes...' name='notes' />
         </div>
-
-
+        </div>
       <button type='submit' className='cc-button'>Create Customer</button>
     </form>
   )
--- react-web/src/components/JobsList.js
+++ react-web/src/components/JobsList.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Job from './Job'
 import Collapsible from 'react-collapsible';
-import JobInfo from './JobInfo'
+import JobInfo from './JobInfo' // ?
 
 export default function JobsList({
 items, username
@@ -12,7 +12,8 @@ items, username
     <div>
       {
         items.map(item => (
-          <Collapsible trigger={ item.deliveryStreet + item.deliverySuburb + item.deliveryTime + item.businessId }>
+          // eslint-disable-next-line
+          <Collapsible trigger={ item.deliveryStreet + '  ' + item.deliverySuburb + '  ' + item.deliveryTime + '  ' + '|' + '  ' + item.businessId }>
             <Job key={item._id} { ...item } username={ username } />
           </Collapsible>
         ))
